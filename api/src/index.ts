aec71bfe70bda4fa359a8c36a02ce6b4dde05175
chris <mr.chrisnash@gmail.com>
2023-07-20 14:44:08 -0700

    connect api to local mysql server, display post data on client index page

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const db = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  database: process.env.DB_NAME || 'bloghub',
+  password: process.env.DB_PASSWORD,
 });
+
+app.get('/posts', async (req, res) => {
+  const [rows] = await db.query('SELECT * FROM posts');
+  res.send({ data: rows, message: 'all entries from posts table' });
+});
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import { Post } from '../types/Post';
+
 function IndexPage() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:5000/posts");
+      const data = await response.json();
+      console.log(data)
+      setPosts(data.data);
+    };
+    fetchData();
+  }, []);
+
+
   return (
-    <div>IndexPage</div>
-  )
+    <div>
+      <h1>Posts</h1>
+      {posts.map((post: Post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default IndexPage
+export default IndexPage;
