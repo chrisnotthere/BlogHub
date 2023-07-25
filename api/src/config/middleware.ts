f6ccaea813017e59e227ecfdd83a94c81b06b3f1
chris <mr.chrisnash@gmail.com>
2023-07-25 11:19:21 -0700

    update post interface, add multer and static images middleware

--- api/src/config/middleware.ts
+++ api/src/config/middleware.ts
@@ -1,6 +1,10 @@
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+import multer from "multer";
+
+// set images folder as destination for multer
+export const upload = multer({ dest: 'images/' });
 
 export const applyMiddleware = (app: express.Application) => {
   app.use(
@@ -11,4 +15,5 @@ export const applyMiddleware = (app: express.Application) => {
   );
   app.use(express.json());
   app.use(cookieParser());
+  app.use('/images', express.static('images')); // serve images from images folder
 };
