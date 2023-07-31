a9853e870465255b77754979c53c337da7920186
chris <mr.chrisnash@gmail.com>
2023-07-31 15:45:21 -0700

    comment/rate section

--- client/src/pages/PostPage.tsx
+++ client/src/pages/PostPage.tsx
@@ -114,6 +114,8 @@ function PostPage() {
 
   // fetch user rating
   const fetchPostRating = async () => {
+    if (userInfo.isLoggedIn === false) return;
+
     const response = await fetch(
       `http://localhost:5000/rating/${id}/${userInfo.user_id}`
     );
@@ -167,6 +169,9 @@ function PostPage() {
       <div className={styles.postTitle}>
         <h2>{post.title}</h2>
       </div>
+      <div className={styles.postAuthor}>
+        <p>Posted By: {post.author}</p>
+      </div>
       <div className={styles.icons}>
         <svg
           onClick={handleEdit.bind(null, post.id)}
@@ -206,53 +211,55 @@ function PostPage() {
         }}
       />
 
-      <div className={styles.postRatingContainer}>
-        <div className={styles.postAverageRating}>
+      <div className={styles.postFoot}>
+        <div className={styles.createCommentContainer}>
+          <p>post a comment section</p>
+        </div>
+        <div className={styles.postRatingContainer}>
+          <div className={styles.postAverageRating}>
+            <p>
+              Post Rating:{" "}
+              {avgRating ? avgRating.toFixed(2) : "No ratings yet"}
+            </p>
+          </div>
+          <div className={styles.postRating}>
+            <p>Rate this post:</p>
+            <div className={styles.postRatingStars}>
+              {[1, 2, 3, 4, 5].map((value) => (
+                <svg
+                  key={value}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill={value <= (rating || 0) ? "gold" : "none"}
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="postRatingStar"
+                  data-value={value}
+                  onClick={(e) =>
+                    setRating(Number((e.target as HTMLElement).dataset.value))
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                  />
+                </svg>
+              ))}
+            </div>
+            <button onClick={submitRating} type="button">
+              Submit Rating
+            </button>
+          </div>
           <p>
-            Average Rating:{" "}
-            {avgRating ? avgRating.toFixed(2) : "No ratings yet"}
+            Your rating:{" "}
+            {userRating
+              ? `${userRating} stars`
+              : "N/A"}
           </p>
         </div>
-        <div className={styles.postRating}>
-          <p>Rate this post:</p>
-          <div className={styles.postRatingStars}>
-            {[1, 2, 3, 4, 5].map((value) => (
-              <svg
-                key={value}
-                xmlns="http://www.w3.org/2000/svg"
-                fill={value <= (rating || 0) ? "gold" : "none"}
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="postRatingStar"
-                data-value={value}
-                onClick={(e) =>
-                  setRating(Number((e.target as HTMLElement).dataset.value))
-                }
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                />
-              </svg>
-            ))}
-          </div>
-          <button onClick={submitRating} type="button">
-            Submit Rating
-          </button>
-        </div>
-        <p>
-          Your rating:{" "}
-          {userRating
-            ? `${userRating} stars`
-            : "You have not rated this post yet"}
-        </p>
       </div>
 
-      <div className={styles.postAuthor}>
-        <p>Posted By: {post.author}</p>
-      </div>
     </div>
   );
 }
