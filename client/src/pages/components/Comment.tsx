b6b68548d42b766f0811dc6be85f9427bc3166fa
chris <mr.chrisnash@gmail.com>
2023-08-01 10:04:47 -0700

    post a comment section

--- client/src/pages/components/Comment.tsx
+++ client/src/pages/components/Comment.tsx
@@ -1,19 +1,39 @@
-import styles from "../../assets/styles/postpage.module.css";
+import ReactQuill from "react-quill";
+import styles from "../../assets/styles/comment.module.css";
 
 interface CommentComponentProps {
   id: string | undefined;
   userInfo: any;
 }
 
-export default function CommentComponent({id, userInfo}: CommentComponentProps) {
-  
-  // console.log(userInfo)
+export default function CommentComponent({
+  id,
+  userInfo,
+}: CommentComponentProps) {
+  const modules = {
+    toolbar: [["bold", "italic", "underline", "strike"], ["clean"]],
+  };
 
-  
+  async function createComment(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    console.log("comment created");
+  }
+
   return (
     <div className={styles.createCommentContainer}>
-    <p>post a comment section</p>
-    <p>123</p>
-  </div>
-  )
+      <form className={styles.commentForm} onSubmit={createComment}>
+        <h3>Post a comment</h3>
+        <div className={styles.editorContainer}>
+          <ReactQuill
+            className={styles.commentEditor}
+            // value={value}
+            theme={"snow"}
+            // onChange={onChange}
+            modules={modules}
+          />
+        </div>
+        <button type="submit">Comment</button>
+      </form>
+    </div>
+  );
 }
