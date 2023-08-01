e8304bad6948446d26fb6b0e592054cb7f36f5e6
chris <mr.chrisnash@gmail.com>
2023-08-01 11:17:04 -0700

    comment model

---
+++ api/src/models/comment.model.ts
@@ -0,0 +1,15 @@
+export interface Comment {
+  id: number;
+  post_id: number;
+  user_id: number;
+  content: string;
+  likes: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface CommentLike {
+  id: number;
+  user_id: number;
+  comment_id: number;
+}
