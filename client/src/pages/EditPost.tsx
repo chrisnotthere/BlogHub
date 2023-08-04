c302350789e7bde1d2c5ee34d812376c62b871fd
chris <mr.chrisnash@gmail.com>
2023-08-04 11:25:53 -0700

    edit page updated to match updated create page

--- client/src/pages/EditPost.tsx
+++ client/src/pages/EditPost.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useParams, Navigate } from "react-router-dom";
 import Editor from "./components/Editor";
 import styles from "../assets/styles/create-post.module.css";
@@ -8,10 +8,14 @@ function EditPost() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [image, setImage] = useState<FileList | null>(null);
+  const [existingImage, setExistingImage] = useState<string | null>(null);
+  const [useDefaultImage, setUseDefaultImage] = useState(false);
   const [redirect, setRedirect] = useState(false);
+  const [contentError, setContentError] = useState("");
 
   const { id } = useParams();
   const { userInfo } = useContext(UserContext);
+  const fileInputRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,22 +23,43 @@ function EditPost() {
       if (response.ok) {
         const responseJson = await response.json();
         const data = responseJson.data;
+        console.log(data)
+
         setTitle(data.title || "");
         setContent(data.content || "");
+        setExistingImage(data.image || null); 
+
       }
     };
 
     fetchData();
   }, [id]);
 
+  const handleUseDefaultImage = () => {
+    setUseDefaultImage(true);
+    setImage(null);
+
+    if (fileInputRef.current) {
+      (fileInputRef.current as any).value = null;
+    }
+  };
+
   async function updatePost(e: { preventDefault: () => void }) {
     e.preventDefault();
+
+    if (!content.trim()) {
+      setContentError("Blog content cannot be empty.");
+      return;
+    }
+
     const formData = new FormData();
     formData.set("title", title);
     formData.set("content", content);
     formData.set("author", userInfo.username);
 
-    if (image) {
+    if (useDefaultImage) {
+      formData.set("image", "images/default.webp");
+    } else if (image) {
       formData.set("image", image[0]);
     }
 
@@ -68,14 +93,32 @@ function EditPost() {
           required
         />
         <label htmlFor="image">Cover Image</label>
+
+        <p className={styles.currentImage}>Current Cover Image: {existingImage || 'Default Image'}</p>
+        
         <input
+          ref={fileInputRef}
           type="file"
-          onChange={(ev) => setImage(ev.target.files)}
+          onChange={(ev) => {
+            setImage(ev.target.files);
+            setUseDefaultImage(false);
+          }}
           id="image"
         />
+
+        <div className={styles.defaultContainer}>
+          <button type="button" onClick={handleUseDefaultImage}>
+            Use Default Image
+          </button>
+          <p>{useDefaultImage ? "Default image selected" : ""}</p>
+        </div>
+
         <div className={styles.quillContainer}>
           <Editor value={content} onChange={setContent} />
         </div>
+
+        {contentError && <p className={styles.contentErrorMessage}>{contentError}</p>}
+
         <button>Update post</button>
       </form>
     </div>
