b4648a3b663b4385447c6c8f70bd04f4b1bd44ee
chris <mr.chrisnash@gmail.com>
2023-08-11 12:15:44 -0700

    update port

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,7 +6,8 @@ const app = express();
 applyMiddleware(app);
 app.use(routes);
 
-const port = 5000;
+// Use Heroku's assigned port, or 5000 if running locally
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
