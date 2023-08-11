071d53ee2671a4bc7a14a3891ffd97c9bd3ba9fb
chris <mr.chrisnash@gmail.com>
2023-08-11 13:05:49 -0700

    connect to JAWSDB

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,14 +1,20 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import { URL } from "url";
+
 dotenv.config({ path: ".env.local" });
 
-const connectionConfig = process.env.JAWSDB_URL
-  ? { url: process.env.JAWSDB_URL }
-  : {
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      database: process.env.DB_NAME || "bloghub",
-      password: process.env.DB_PASSWORD,
-    };
+if (process.env.JAWSDB_URL) {
+  const jawsDBUrl = new URL(process.env.JAWSDB_URL);
+  process.env.DB_HOST = jawsDBUrl.hostname;
+  process.env.DB_USER = jawsDBUrl.username;
+  process.env.DB_PASSWORD = jawsDBUrl.password;
+  process.env.DB_NAME = jawsDBUrl.pathname.substring(1);
+}
 
-export const db = mysql.createPool(connectionConfig);
+export const db = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  database: process.env.DB_NAME || "bloghub",
+  password: process.env.DB_PASSWORD,
+});
