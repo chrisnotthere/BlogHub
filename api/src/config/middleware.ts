16de9b0050326d7d40832b26936f6e83ed0f2deb
chris <mr.chrisnash@gmail.com>
2023-08-11 14:28:59 -0700

    CORS

--- api/src/config/middleware.ts
+++ api/src/config/middleware.ts
@@ -4,16 +4,16 @@ import cookieParser from "cookie-parser";
 import multer from "multer";
 
 // set images folder as destination for multer
-export const upload = multer({ dest: 'images/' });
+export const upload = multer({ dest: "images/" });
 
 export const applyMiddleware = (app: express.Application) => {
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "https://chrisnotthere.github.io/BlogHub"],
       credentials: true, // server accepts cookies from the client
     })
   );
   app.use(express.json());
   app.use(cookieParser());
-  app.use('/images', express.static('images')); // serve images from images folder
+  app.use("/images", express.static("images")); // serve images from images folder
 };
