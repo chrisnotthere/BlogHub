d84b260703d5d1098315d8c31726a5b1fcab573e
chris <mr.chrisnash@gmail.com>
2023-08-13 16:31:48 -0700

    update cookie, sameSite: none

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -62,7 +62,7 @@ export const loginUserController = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
       maxAge: 3600000,
     });
