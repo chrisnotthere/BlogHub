04b5435effd8e47cc9e8307f9a112781b3ddb863
chris <mr.chrisnash@gmail.com>
2023-08-13 14:44:02 -0700

    deploy issues

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </UserContextProvider>
