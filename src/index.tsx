afa186b4ae440809827aeff3a713470c74514f9b
chris <mr.chrisnash@gmail.com>
2023-08-13 15:51:14 -0700

    deploy issues

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/styles/index.css";
 import App from "./pages/App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { UserContextProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-console.log('process.env.REACT_APP_PUBLIC_URL');
+console.log("process.env.REACT_APP_PUBLIC_URL");
 console.log(process.env.REACT_APP_PUBLIC_URL);
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
-        <App />
-      </BrowserRouter>
+      {/* <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}> */}
+        <HashRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+          <App />
+        </HashRouter>
+      {/* </BrowserRouter> */}
     </UserContextProvider>
   </React.StrictMode>
 );
