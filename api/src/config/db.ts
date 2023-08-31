9eef6bbc329b71dc5c4fc3e076f67196fc80d465
chris <mr.chrisnash@gmail.com>
2023-08-31 09:44:10 -0700

    added docker db config. excluded truncate_all from running

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -4,6 +4,15 @@ import { URL } from "url";
 
 dotenv.config({ path: ".env.local" });
 
+// docker db
+if (process.env.DOCKER_DB) {
+  process.env.DB_HOST = "db";
+  process.env.DB_USER = "root";
+  process.env.DB_PASSWORD = "password";
+  process.env.DB_NAME = "blogdb";
+}
+
+// production db
 if (process.env.JAWSDB_URL) {
   const jawsDBUrl = new URL(process.env.JAWSDB_URL);
   process.env.DB_HOST = jawsDBUrl.hostname;
@@ -12,6 +21,7 @@ if (process.env.JAWSDB_URL) {
   process.env.DB_NAME = jawsDBUrl.pathname.substring(1);
 }
 
+// local db
 export const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
