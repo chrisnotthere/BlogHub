845f3e0440bf4e81e6855757e10c06ca8841b5e9
chris <mr.chrisnash@gmail.com>
2024-02-26 09:30:54 -0800

    pagination component ui revamp

--- client/src/pages/components/Pagination.tsx
+++ client/src/pages/components/Pagination.tsx
@@ -1,5 +1,3 @@
-import styles from "../../assets/styles/indexpage.module.css";
-
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
@@ -12,19 +10,21 @@ function Pagination({
   setCurrentPage,
 }: PaginationProps) {
   return (
-    <div className={styles.paginationContainer}>
+    <div className="flex justify-center items-center gap-4 mx-auto mt-4 sm:mt-0 py-2 px-4 max-w-md border-2 border-bright-teal rounded-lg bg-white">
       <button
         onClick={() => setCurrentPage(currentPage - 1)}
         disabled={currentPage === 1 || totalPages === 0}
+        className="text-xs md:text-base border border-bright-teal rounded-lg py-2 px-4 cursor-pointer transition-colors duration-300 ease-linear hover:bg-soft-mint disabled:opacity-50 disabled:cursor-not-allowed"
       >
         Previous
       </button>
-      <span>
+      <span className="text-xs md:text-base">
         Page: {totalPages === 0 ? 0 : currentPage} of {totalPages}
       </span>
       <button
         onClick={() => setCurrentPage(currentPage + 1)}
         disabled={currentPage === totalPages || totalPages === 0}
+        className="text-xs md:text-base border border-bright-teal rounded-lg py-2 px-4 cursor-pointer transition-colors duration-300 ease-linear hover:bg-soft-mint disabled:opacity-50 disabled:cursor-not-allowed"
       >
         Next
       </button>
